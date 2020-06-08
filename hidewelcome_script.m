a34fcee1a26ecd0fc79ffdd489a20c5a40bd590d
Sunil K. Kenchanmane Raju <34524287+Kenchanmane-Raju@users.noreply.github.com>
2020-06-07 23:07:54 -0400

    Delete hidewelcome_script.m
    str

--- hidewelcome_script.m
+++
@@ -1,7 +0,0 @@
-%hidewelcome_script.m
-% This script hides all of the objects on the welcome screen
-
-set(handles.welcome_text,'Visible','off');
-set(handles.welcomedescription_text,'Visible','off');
-set(handles.newproject_pushbutton,'Visible','off');
-set(get(handles.axes5,'children'),'Visible','off');
