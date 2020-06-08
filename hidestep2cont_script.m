b6e7b2d4c0482c5e02ce6e5dad9a8f127ddaec88
Sunil K. Kenchanmane Raju <34524287+Kenchanmane-Raju@users.noreply.github.com>
2020-06-07 23:05:38 -0400

    Delete hidestep2cont_script.m
    str

--- hidestep2cont_script.m
+++
@@ -1,7 +0,0 @@
-%hidestep2_script.m
-%   This script hides the objects from step 2
-
-% hide objects from the inital frame selection step [Step 2]
-set(handles.initialframe_slider,'Visible','off');
-set(handles.continue2_pushbutton,'Visible','off');
-set(handles.undo2_pushbutton,'Visible','off');
