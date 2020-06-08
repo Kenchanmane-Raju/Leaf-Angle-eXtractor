c0d8c398577b2bfd075315a1195e2b74a145507c
Sunil K. Kenchanmane Raju <34524287+Kenchanmane-Raju@users.noreply.github.com>
2020-06-07 23:08:34 -0400

    Delete initialframe_func.m
    str

--- initialframe_func.m
+++
@@ -1,8 +0,0 @@
-function [ img ] = initialframe_func( i, img_array )
-%initialframe_func
-%   uses input framecount and img_array to output the corresponding image
-i = round(i);
-img = img_array{i};
-
-end
-
