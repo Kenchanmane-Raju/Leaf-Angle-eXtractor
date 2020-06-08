d67a7d706ba7b185314305dfc012ff62ff48aaf3
Sunil K. Kenchanmane Raju <34524287+Kenchanmane-Raju@users.noreply.github.com>
2020-06-07 23:06:21 -0400

    Delete hidestep4_script.m
    str

--- hidestep4_script.m
+++
@@ -1,8 +0,0 @@
-%hidestep4_script.m
-%   This script hides the objects from step 4
-
-% hide objects from the select leaves step
-set(handles.continue4_pushbutton,'Visible','off');
-set(handles.undo4_pushbutton,'Visible','off');
-
-
